cb5717ff54b472dad75766d323ff2b85a1508bf4
wpng3 <kinareevyarik@gmail.com>
2025-10-27 08:40:00 -0300

    заливаю наработки сервера

---
+++ mysite/Main/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here
+def main(request):
+    return render(request, 'main.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def catalog(request):
+    return render(request, 'catalog.html')
